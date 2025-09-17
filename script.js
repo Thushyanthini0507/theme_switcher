c7595deb961869bbc80595b1f221d9b41d7b5545
Thushyanthini0507 <thushyanthini0507@gmail.com>
2025-09-17 06:21:08 +0530

    script changes and find the error

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function theme() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
+const themeSelector = document.getElementById("theme-name");
+
+// Load saved theme
+const savedTheme = localStorage.getItem("theme-name");
+
+if (savedTheme) {
+  document.body.className = savedTheme;
+  themeSelector.value = savedTheme;
+} else {
+  document.body.className = "light"; // default
 }
+
+// Change theme
+themeSelector.addEventListener("change", function () {
+  document.body.className = this.value;
+  localStorage.setItem("theme-name", this.value);
+});
